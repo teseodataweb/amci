3364c7752e9d10bf1f26d1b327466b147d4274c4
RedNova555 <petitkarol555@gmail.com>
2025-07-04 17:23:54 -0600

    040 Contacto - mapa

--- src/components/contacto/ContactMap.tsx
+++ src/components/contacto/ContactMap.tsx
@@ -5,7 +5,7 @@ const ContactMap = () => {
          <div className="row">
             <div className="col-xxl-12">
                <div className="contact-map">
-                  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d4216.433331900906!2d90.36996032419312!3d23.83718617432321!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1693682874850!5m2!1sen!2sbd"></iframe>
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3596.876691715366!2d-100.32448932400735!3d25.642217713530865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8662be437f0b09b7%3A0xdde89a0191d772a!2sDavid%20Alfaro%20Siqueiros%20106-SUITE%201700%20P.17%2C%20Valle%20Oriente%2C%2066269%20San%20Pedro%20Garza%20Garc%C3%ADa%2C%20N.L.%2C%20Mexico!5e0!3m2!1sen!2sbd!4v1751671350791!5m2!1sen!2sbd"></iframe>
                </div>
             </div>
          </div>
